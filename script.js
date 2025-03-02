bca7aa5b236500d9bd47426a3c85a3bd543d3d7e
witeackerjnr <124600702+witeackerjnr@users.noreply.github.com>
2025-03-02 14:15:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+    // Check if dark mode is stored in localStorage
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        $("body").addClass("dark-mode");
+    }
+
     $(".toggle-btn").click(function() {
         $("body").toggleClass("dark-mode");
+
+        if ($("body").hasClass("dark-mode")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+        }
     });
+
+    $("body").css("display", "none").fadeIn(800);
 });
